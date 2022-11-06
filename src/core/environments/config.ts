a0cd27505d2221eb28b6e4682ad46312dec0929f
Cartago <cartago.relaxingcup@gmail.com>
2022-11-06 15:39:12 +0100

    9
     seguimos intentando omitir dev en produccion en railway

--- src/core/environments/config.ts
+++ src/core/environments/config.ts
@@ -1,15 +1,19 @@
 import dotenv from "dotenv";
+import { colors } from "../../helpers/console-log.helper";
 
 const dotEnvResult = dotenv.config({
-	path:
-		__dirname +
-		"/.env" 
-		// +
-		// (process.env.NODE_ENV === "production" ? ".prod" : ""),
+	path: __dirname + "/.env",
+	// +
+	// (process.env.NODE_ENV === "production" ? ".prod" : ""),
 });
 if (dotEnvResult.error) {
-	console.log("Get an error looking for the environment variables");
-	throw dotEnvResult.error;
+	console.log(
+		colors.fg.red +
+			"Get an error looking for the environment variables" +
+			colors.reset
+	);
+	// throw dotEnvResult.error;
+	console.log(colors.fg.yellow + dotEnvResult.error + colors.reset);
 }
 
 //? Si no se encuentran las variables de entorno usa las variables de testing
