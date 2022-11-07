0ce0e32b5f81bd0fa682d15a562064ed15d3c393
Cartago <cartago.relaxingcup@gmail.com>
2022-11-07 20:21:56 +0100

    volvemos a cambiar el index.js para deta a comonjs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //? ARCHIVO UNICAMENTE CREADO PARA HACER DE ENLACE CON DETA
-import app from "/dist/core/index.js";
+// import app from "/dist/core/index.js";
+const app = require("/dist/core/index.js");
 
 module.exports = app;
